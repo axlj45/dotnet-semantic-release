271a9eb3f183f158c3d119d962ca987359db10f0
AJ Leonard <axlgh45@gmail.com>
2018-10-23 12:38:28 -0400

    fix(ProjectReleaser): Fixed relative git path issuee

--- src/AxlSoft.SemanticRelease.CommitAnalyzer/ProjectReleaser.cs
+++ src/AxlSoft.SemanticRelease.CommitAnalyzer/ProjectReleaser.cs
@@ -19,7 +19,10 @@ namespace AxlSoft.SemanticRelease.CommitAnalyzer
         {
             var repo = _gitRepo.GetRepositoryReference<Repository>();
 
-            repo.Index.Add(_project.ProjectPath.ReplaceFirst(Directory.GetParent(_gitRepo.RepositoryPath).FullName + "/", ""));
+            int workDirLength = Directory.GetParent(_gitRepo.RepositoryPath).FullName.Length;
+            string workingPath = Path.GetFullPath(_project.ProjectPath).Substring(workDirLength + 1);
+
+            repo.Index.Add(workingPath);
             var signature = new Signature("jenkins", "jenkins", DateTimeOffset.UtcNow);
             var vCommit = repo.Commit($"chore(release): Releasing {_project.GetVersion()}", signature, signature);
             var vTag = repo.ApplyTag(_project.GetVersion());
